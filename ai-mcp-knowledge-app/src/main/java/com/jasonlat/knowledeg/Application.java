f49c95ce51bef1924bd459b04c6d3e0cac1fed82
jasonlat-dot <lijiaqiangli1024@gmail>
2025-08-23 14:25:10 +0800

    feat: update project

---
+++ ai-mcp-knowledge-app/src/main/java/com/jasonlat/knowledeg/Application.java
@@ -0,0 +1,23 @@
+package com.jasonlat.knowledeg;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@Slf4j
+@Configurable
+@SpringBootApplication
+@SpringBootConfiguration
+@EnableEncryptableProperties
+@ComponentScan(basePackages = {"com.jasonlat", "cc.jq1024.middleware"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("ai mcp knowledge 启动成功......");
+    }
+
+}
